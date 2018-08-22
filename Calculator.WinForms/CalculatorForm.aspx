82f543f0dc1e1b2b534121d4258fd1ac0a33e12d
Avinash M <avinashm201993@gmail.com>
2018-08-22 18:14:25 -0500

    Code is implemented using class library

---
+++ Calculator.WinForms/CalculatorForm.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CalculatorForm.aspx.cs" Inherits="Calculator.WinForms.CalculatorForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <div>
+                <asp:Label ID="lblNumber1" runat="server" Text="Number 1: "></asp:Label>
+                <asp:TextBox ID="txtNumber1" runat="server"></asp:TextBox>
+            </div>
+            <div>
+                <asp:Label ID="lblNumber2" runat="server" Text="Number 2: "></asp:Label>
+                <asp:TextBox ID="txtNumber2" runat="server"></asp:TextBox>
+            </div>
+            <div>
+                <asp:RadioButtonList ID="rbtnOperator" runat="server" RepeatDirection="Horizontal"></asp:RadioButtonList>
+            </div>
+            <div>
+                <asp:Button ID="btnCalculate" runat="server" Text="Calculate" OnClick="btnCalculate_Click" />
+                <asp:Label ID="lblResult" runat="server" Text=""></asp:Label>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Calculator.WinForms/CalculatorForm.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Web.UI;
+using MathLibrary;
+
+namespace Calculator.WinForms
+{
+    public partial class CalculatorForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!Page.IsPostBack)
+            {
+                rbtnOperator.Items.Add("+");
+                rbtnOperator.Items.Add("-");
+                rbtnOperator.Items.Add("*");
+                rbtnOperator.Items.Add("/");
+                rbtnOperator.Items.Add("%");
+            }
+        }
+
+        protected void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double firstNumber = Convert.ToDouble(txtNumber1.Text);
+            double secondNumber = Convert.ToDouble(txtNumber2.Text);
+
+            double result = 0.0D;
+
+            switch (rbtnOperator.Text)
+            {
+                case "+":
+                    result = MathOperations.Addition(firstNumber, secondNumber);
+                    break;
+                case "-":
+                    result = MathOperations.Subtraction(firstNumber, secondNumber);
+                    break;
+                case "*":
+                    result = MathOperations.Multiplication(firstNumber, secondNumber);
+                    break;
+                case "/":
+                    result = MathOperations.Division(firstNumber, secondNumber);
+                    break;
+                case "%":
+                    result = MathOperations.Remainder(firstNumber, secondNumber);
+                    break;
+                default:
+                    break;
+            }
+
+            lblResult.Text = result.ToString();
+        }
+    }
+}
